81c42e78b7fdb66c8f18e4ef925f99b49b3ff37a
paola <baldopaola01@gmail.com>
2019-08-04 11:31:19 -0300

    fix

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,22 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
 
-}]);
+.controller('View1Ctrl', function($scope, $http) {
+
+	$scope.performCall = function(){
+    // Simple GET request example:
+    $http({
+      method: 'GET',
+      url: 'http://localhost:8080/beers/random'
+    }).then(function successCallback(response) {
+    	$scope.greeting = response.data;
+       // this callback will be called asynchronously
+       // when the response is available
+    }, function errorCallback(response) {
+       // called asynchronously if an error occurs
+       // or server returns response with an error status.
+    });
+	}
+
+});
